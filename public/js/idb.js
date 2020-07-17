1857a7d0112838f522f6ab9f87f8ac9e4d21bbde
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-07-17 15:27:41 -0700

    latest changes

--- public/js/idb.js
+++ public/js/idb.js
@@ -3,7 +3,7 @@ let db;
 // establish a connection to IndexedDB database called 'budget_tracker' and set it to version 1
 const request = indexedDB.open('budget_tracker', 1);
 
-// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+// this event will emit if the database version changes (non-existent to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function(event) {
     // save a reference to the database 
     const db = event.target.result;
@@ -13,15 +13,12 @@ request.onupgradeneeded = function(event) {
 
 // upon a successful
 request.onsuccess = function(event) {
-    // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+    // when db is successfully created with its object store (from onupgradeneeded event above) or simply established a connection, save reference to db in global variable
     db = event.target.result;
   
     //check if app is online, if yes, run uploadTransaction() function to send all local db data to api
     if(navigator.onLine) {
         //we haven't created this yet
-        // populateTotal();
-        // populateTable();
-        // populateChart();
         uploadTransaction();
     }
 };
--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -21,7 +21,7 @@ const CACHE_NAME = APP_PREFIX + VERSION;
 self.addEventListener('install', function(e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function(cache) {
-            console.log('intalling cache: ' + CACHE_NAME)
+            console.log('installing cache: ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
     )
